ec6f429dd749ed7b03f00941872c05f4ec154c5b
YJ <yair.yoseph@gmail.com>
2018-10-04 11:23:11 -0700

    retaurant side scroll - needs to be able to scroll with mouse left and right

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -19,11 +19,18 @@ $("#expand-btn").on("click", function(){
     $('#expanded-modal').modal('show'); 
 });
 
+//Nav bar
 $("#navbar-log-sign").on("click", function(){
     $('#log-sign-modal').modal('show'); 
 });
 
 
+
+//Back button from expand to returant modal
+$("#backto-scroll-modal").on("click", function(){
+    $('#expanded-modal').modal('hide'); 
+    $('#exampleModalCenter').modal('show'); 
+});
 
 
 
