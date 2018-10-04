f7c555159b4e9cd7a3cda5bf06aebf1c0434e796
majorazero <daniel.cw.hsu@gmail.com>
2018-10-04 11:21:10 -0700

    made yelpendpoing return something

--- controllers/yelpRouter.js
+++ controllers/yelpRouter.js
@@ -1,12 +1,13 @@
 const request = require("request");
 module.exports = function(app){
   app.get("/api/yelp",function(req,res){
-    request("https://api.yelp.com/v3/businesses/search?location=newyork&term=food",{
+    request("https://api.yelp.com/v3/businesses/search?location=newyork&term=mcdonald",{
       headers : {
         Authorization: "Bearer g9EeN1YvpMFGmbPjwKqrAVIVoCCCCK0g-uyPyF9nJigZOIKQpJYJa2S3FOUhlJ9Y6cnJszqMSRFjwrobfelVeALnWAHR1uBlC2L9fVkWZh0-sfqrvRZYjecDMWGRW3Yx"
       }
     },function(error,response,body){
       console.log(body);
+      //we'll need id (to not repeat entries), name, address, price, img-link
       res.json(JSON.parse(body));
     });
   });
