ed4770c19eb6154c8079b9c7d9d812c4009b6e3b
YJ <yair.yoseph@gmail.com>
2018-10-04 14:52:39 -0700

    modal2 fixes and scrollig adjustment

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,39 +1,40 @@
 
-$("#group-name1").on("click", function(){
-    $('#exampleModalCenter').modal('show'); 
+$("#group-name1").on("click", function () {
+    $('#exampleModalCenter').modal('show');
 });
 
 
-$("#group-name2").on("click", function(){
-    $('#exampleModalCenter').modal('show'); 
+$("#group-name2").on("click", function () {
+    $('#exampleModalCenter').modal('show');
 });
 
 
-$("#group-name3").on("click", function(){
-    $('#exampleModalCenter').modal('show'); 
+$("#group-name3").on("click", function () {
+    $('#exampleModalCenter').modal('show');
 });
 
-
-$("#expand-btn").on("click", function(){
-    $('#exampleModalCenter').modal('hide'); 
-    $('#expanded-modal').modal('show'); 
+//Top 5 choices modal
+$("#expand-btn").on("click", function () {
+    $("body").css("overflow", "hidden");
+    $('#exampleModalCenter').modal('hide');
+    $('#expanded-modal').modal('show');
 });
 
 //Nav bar
-$("#navbar-log-sign").on("click", function(){
-    $('#log-sign-modal').modal('show'); 
+$("#navbar-log-sign").on("click", function () {
+    $('#log-sign-modal').modal('show');
 });
 
 
 
 //Back button from expand to returant modal
-$("#backto-scroll-modal").on("click", function(){
-    $('#expanded-modal').modal('hide'); 
-    $('#exampleModalCenter').modal('show'); 
+$("#backto-scroll-modal").on("click", function () {
+    $('#expanded-modal').modal('hide');
+    $('#exampleModalCenter').modal('show');
 });
 
-    $('.scrollmenu').mousewheel(function(e, delta) {
+$('.scrollmenu').mousewheel(function (e, delta) {
     this.scrollLeft -= (delta * 1);
     e.preventDefault();
-    });
+});
 
