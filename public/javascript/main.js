375403e96a211bc9389116b1ccc4ae0225790037
majorazero <daniel.cw.hsu@gmail.com>
2018-10-04 14:08:07 -0700

    added some docstrings

--- public/javascript/main.js
+++ public/javascript/main.js
@@ -18,16 +18,22 @@ $("#expand-btn").on("click", function(){
     $('#exampleModalCenter').modal('hide');
     $('#expanded-modal').modal('show');
 });
-
+/**
+* Modal event handler for the login button.
+*/
 $("#navbar-log-sign").on("click", function(){
   $('#log-sign-modal').modal('show');
 });
-
+/**
+* Modal event handler for the flag button.
+*/
 $("#flagModButts").on("click", function(){
   $("#exampleModalCenter").modal("hide");
   $("#flagModal").modal("show");
 });
-
+/**
+* Modal event handler for the nav invite code button.
+*/
 $("#inviteCode").on("click",function(){
   $("#inviteCodeModal").modal("show");
 })
