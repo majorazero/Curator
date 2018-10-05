9ab4eae59cf5d0f6090c3ff9c13051c7c30ef10d
majorazero <daniel.cw.hsu@gmail.com>
2018-10-04 18:49:28 -0700

    added post new user route

--- controllers/userRouter.js
+++ controllers/userRouter.js
@@ -4,16 +4,39 @@ module.exports = function(app){
   ////////////////////////////////////
   //////Read Routes
   ///////////////////////////////////
-  //fetches all users.
-  app.get("/api/allUser",function(req,res){
-    db.User.findAll({}).then(function(data){
-      res.json(data);
+  //get user by userName
+  app.get("/api/user/:username/:password",function(req,res){
+    console.log(req.body);
+    db.User.findOne({
+      where: {
+        username: req.params.username
+      }
+    }).then(function(data){
+      //should wrap this in a password check
+      if (data === null){
+        res.json("Oops, didn't exist.");
+      }
+      else {
+        if(req.params.password === data.password){
+          res.json(data);
+        }
+        else {
+          res.json("Oops, bad password.");
+        }  
+      }
     });
   });
   ////////////////////////////////////
   //////Create Routes
   ///////////////////////////////////
-
+  app.post("/api/newUser",function(req,res){
+    db.User.create({
+      username: req.body.username,
+      password: req.body.password
+    }).then(function(){
+      res.json("Made it!");
+    });
+  });
   ////////////////////////////////////
   //////Update Routes
   ///////////////////////////////////
@@ -21,5 +44,5 @@ module.exports = function(app){
   ////////////////////////////////////
   //////Destroy Routes
   ///////////////////////////////////
-  
+
 };
