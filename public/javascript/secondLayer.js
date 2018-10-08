7fbdbf90ef114d242cd3005a464b966354f4fc2a
majorazero <daniel.cw.hsu@gmail.com>
2018-10-07 23:37:53 -0700

    added no entries catch for secondlayer

--- public/javascript/secondLayer.js
+++ public/javascript/secondLayer.js
@@ -11,24 +11,29 @@ $("#expand-btn").on("click", function () {
     }).then(function(data){
       console.log(data);
       $("#expanded-scrollMenu").empty();
-      //constructs scrollmenu for expanded
-      for(let i = 0; i < data.length; i++){
-        let outerWrapper = $("<div>").addClass("row group-rest-info");
-        let restPortrait = $("<div>").addClass("col-sm-3 rest-img");
-        $(restPortrait).append("<img src='"+data[i].Restaurant.imageLink+"' class='img-fluid' />");
-        let infoCard = $("<div>").addClass("col-sm-7");
-        $(infoCard).append("<div class='rest-name'>"+data[i].Restaurant.name+"</div>");
-        $(infoCard).append("<div class='rest-address'>"+data[i].Restaurant.address+"</div>");
-        $(infoCard).append("<div class='rest-price' >"+data[i].Restaurant.price+"</div>");
-        //have to figure out how ratings work still
-        let ratingCard = $("<div>").addClass("col-sm-2 rest-rating").text("*****");
-
-        $(outerWrapper).append(restPortrait);
-        $(outerWrapper).append(infoCard);
-        $(outerWrapper).append(ratingCard);
-        $("#expanded-scrollMenu").append(outerWrapper);
+      if(data.length === 0){
+        $("#expanded-scrollMenu").append("<img class='img-fluid' src='images/noEntries.gif' />");
+        $("#expanded-scrollMenu").append("<h2>Oops, no entries!</h2>");
       }
+      else{
+        //constructs scrollmenu for expanded
+        for(let i = 0; i < data.length; i++){
+          let outerWrapper = $("<div>").addClass("row group-rest-info");
+          let restPortrait = $("<div>").addClass("col-sm-3 rest-img");
+          $(restPortrait).append("<img src='"+data[i].Restaurant.imageLink+"' class='img-fluid' />");
+          let infoCard = $("<div>").addClass("col-sm-7");
+          $(infoCard).append("<div class='rest-name'>"+data[i].Restaurant.name+"</div>");
+          $(infoCard).append("<div class='rest-address'>"+data[i].Restaurant.address+"</div>");
+          $(infoCard).append("<div class='rest-price' >"+data[i].Restaurant.price+"</div>");
+          //have to figure out how ratings work still
+          let ratingCard = $("<div>").addClass("col-sm-2 rest-rating").text("*****");
 
+          $(outerWrapper).append(restPortrait);
+          $(outerWrapper).append(infoCard);
+          $(outerWrapper).append(ratingCard);
+          $("#expanded-scrollMenu").append(outerWrapper);
+        }
+      }
       $('#expanded-modal').modal('show');
     });
 
